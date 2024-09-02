8c99e810e861038d5b93eccb69f47994ee0aad16
Dima <dfedorov@jmart.kz>
2024-09-02 22:14:48 +0500

    add aws

---
+++ aws/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-central-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0de02246788e4a354"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "test-terraform"
+  }
+}
